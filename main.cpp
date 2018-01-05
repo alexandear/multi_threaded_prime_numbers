74af3720754465a3389bff9dac6d44f73bb19ba3
Oleksandr Redko <oleksandr.red+github@gmail.com>
2018-01-05 23:35:39 +0000

    Remove unnecessary code

--- main.cpp
+++ main.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <numeric>
+#include <cmath>
 
 // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 int main()
 {
-    const size_t left = 2;
     const size_t n = 200;
 
     std::vector<bool> a(n + 1, true);
-    std::vector<size_t> natural_numbers(std::size(a));
-    std::iota(std::begin(natural_numbers), std::end(natural_numbers), 0);
-
-    for (size_t i = 2; i * i < n + 1; i++)
+    for (size_t i = 2; i <= std::sqrt(n); i++)
     {
         if (a[i])
         {
-            for (size_t j = i * i; j < n + 1; j += i)
+            for (size_t j = i * i; j <= n; j += i)
             {
                 a[j] = false;
             }
         }
     }
 
-    std::vector<size_t> prime_numbers;
-    for (size_t i = left; i < std::size(natural_numbers); i++)
+    for (size_t i = 2; i < std::size(a); i++)
     {
         if (a[i])
         {
-            prime_numbers.push_back(natural_numbers[i]);
+            std::cout << i << ' ';
         }
     }
-
-    for (auto prime : prime_numbers)
-    {
-        std::cout << prime << ' ';
-    }
-    std::cout << '\n';
 }
